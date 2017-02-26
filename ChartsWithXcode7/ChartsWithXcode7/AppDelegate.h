7ebeefbc1a27bdd71b184f8bf3ed8a1c25d9b09c
sikeersi <271786106@qq.com>
2017-02-26 18:00:25 +0800

    init

---
+++ ChartsWithXcode7/ChartsWithXcode7/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChartsWithXcode7
+//
+//  Created by 许必成 on 2017/2/24.
+//  Copyright © 2017年 Raven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChartsWithXcode7/ChartsWithXcode7/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  ChartsWithXcode7
+//
+//  Created by 许必成 on 2017/2/25.
+//  Copyright © 2017年 Raven. All rights reserved.
+//
+
+import Foundation
